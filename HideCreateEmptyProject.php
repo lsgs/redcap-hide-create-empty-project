176f41c4c75c05073c3c74c360063180c2c1fde5
Luke <lukestevens@hotmail.com>
2022-02-25 11:45:36 +1100

    Move user to role on any page

--- HideCreateEmptyProject.php
+++ HideCreateEmptyProject.php
@@ -1,49 +1,61 @@
 <?php
 /**
  * REDCap External Module: Hide Create Empty Project
- * Hides the "Create an empty project (blank slate)" option when creating or
- * requesting a new project.
+ * - Hides the "Create an empty project (blank slate)" option when creating or requesting a new project thus forcing users to use a template or XML.
+ * - When a dev project has a single user not in a role, and the system-specified default role is present, the user will be added to this role.
  * @author Luke Stevens, Murdoch Children's Research Institute
  */
 namespace MCRI\HideCreateEmptyProject;
 
 use ExternalModules\AbstractExternalModule;
-use REDCap;
 
 class HideCreateEmptyProject extends AbstractExternalModule
 {
-        public function redcap_every_page_top($project_id) {
-                if (PAGE === 'index.php' || PAGE === 'redcap/index.php' || PAGE === 'index.php?action=create') { 
-                        // For "Create new Project" hide "Empty Project" option and default to template
-                        if (isset($_GET['action']) && $_GET['action']==='create') {
-                            ?>
-                            <script type='text/javascript'>
-                                $(document).ready(function() {
-                                    $('input[name="project_template_radio"][value="0"]').closest('div').hide(); 
-                                    $('input[name="project_template_radio"]').val([1]); 
-                                    setTimeout(function() {
-                                        $('input[name="project_template_radio"]').change();
-                                    }, 500);
-                                });
-                            </script>
-                            <?php
-                        }
-                } else if (PAGE === 'ProjectSetup/index.php' && isset($_GET['msg']) && $_GET['msg']==='newproject') {
-                        // When project created, add project users to "Project Owner" role (if template has one)
-                        $defaultRole = $this->getSystemSetting('project-owner-role-name');
-                        if ($defaultRole!=='') {
-                                $sql = "select role_id from redcap_user_roles where project_id=".db_escape(PROJECT_ID)." and role_name='".db_escape($defaultRole)."' limit 1";
-                                $r = db_query($sql);
-                                if ($r->num_rows > 0) {
-                                        $roleId = $r->fetch_assoc()['role_id'];
-                                        $sql = "update redcap_user_rights set role_id=".db_escape($roleId)." where project_id=".db_escape(PROJECT_ID)." and role_id is null";
-                                        $q = db_query($sql);
-
-                                        if (db_affected_rows($q) > 0) {
-                                                REDCap::logEvent('External Module "'.$this->getModuleName().'": Assign default user to default role','role_id = '.$roleId, $sql);
-                                        }
-                                }
-                        }
+    public function redcap_every_page_top($project_id) {
+        if (str_replace(APP_PATH_WEBROOT_PARENT, '', PAGE_FULL)=='index.php' &&
+                isset($_GET['action']) && $_GET['action']==='create') {
+            // For "Create new Project" hide "Empty Project" option and default to template
+            ?>
+            <script type='text/javascript'>
+                $(document).ready(function() {
+                    $('#project_template_radio0').closest('div').hide(); 
+                    setTimeout(function(){
+                        $('#project_template_radio1').val([1]).change();
+                    }, 200);
+                });
+            </script>
+            <?php
+        } else if (isset($_GET['pid']) && $_GET['pid']>0) {
+            // Dev projects force single user into owner role (if template has one) - works more flexibly than query string "new" with new project requests
+            global $Proj;
+            if ($Proj->project['status'] > 0) return;
+            $defaultRole = $this->getSystemSetting('project-owner-role-name');
+            if ($defaultRole!=='') {
+                $defaultRoleId = 0;
+                $r1 = $this->query("select role_id from redcap_user_roles where project_id=? and role_name=? limit 1", [ PROJECT_ID, $defaultRole ]);
+                if ($r1->num_rows > 0) {
+                    while($row = $r1->fetch_assoc()){
+                        $defaultRoleId = $row['role_id'];
+                    }
                 }
+                $r2 = $this->query("select if(role_id is null, 0, 1) as in_role from redcap_user_rights where project_id=? ", [ PROJECT_ID ]);
+                $nInRole = 0;
+                $nUsers = $r2->num_rows;
+                while($row = $r2->fetch_assoc()){
+                    $nInRole += $row['in_role'];
+                }
+                // if default role present and project has single user not in that role, then add them
+                if ($defaultRoleId > 0 && $nUsers==1 && $nInRole==0) {
+                    $this->moveUserToDefaultRole($defaultRoleId);
+                }
+            }
         }
+    }
+
+    protected function moveUserToDefaultRole($defaultRoleId) {
+        $sql = "update redcap_user_rights set role_id=? where project_id=? and role_id is null limit 1"; // already checked project has only one user - the creator/requestor
+        if ($this->query($sql, [ $defaultRoleId, PROJECT_ID ])) {
+            \REDCap::logEvent('External Module "'.$this->getModuleName().'": Assign user to default role','role_id = '.$defaultRoleId, sprintf(str_replace('?','%s',$sql), $defaultRoleId, PROJECT_ID));
+        }
+    }
 }
