16b701a14eb75635da0495b7ba27a38ad46dec1c
Luke <lukestevens@hotmail.com>
2026-01-09 13:14:36 +1100

    New features, enhancements
    - New feature: option for hiding "Add user with custom rights"
    - New feature: repeating option for default role names
    - New feature: highlight when module not enabled for all projects
    - Enhancements to documentation / readme
    - EM framework 16

--- HideCreateEmptyProject.php
+++ HideCreateEmptyProject.php
@@ -12,50 +12,136 @@ use ExternalModules\AbstractExternalModule;
 class HideCreateEmptyProject extends AbstractExternalModule
 {
     public function redcap_every_page_top($project_id) {
-        if (str_replace(APP_PATH_WEBROOT_PARENT, '', PAGE_FULL)=='index.php' &&
-                isset($_GET['action']) && $_GET['action']==='create') {
-            // For "Create new Project" hide "Empty Project" option and default to template
-            ?>
-            <script type='text/javascript'>
-                $(document).ready(function() {
-                    $('#project_template_radio0').closest('div').hide(); 
-                    setTimeout(function(){
-                        $('#project_template_radio1').val([1]).change();
-                    }, 200);
-                });
-            </script>
-            <?php
-        } else if (isset($_GET['pid']) && $_GET['pid']>0) {
+        if (!defined('PAGE')) return;
+        if (empty($project_id)) {
+            if (
+                str_replace(APP_PATH_WEBROOT_PARENT, '', PAGE_FULL)=='index.php' &&
+                isset($_GET['action']) && $_GET['action']==='create') 
+            {
+                // For "Create new Project" hide "Empty Project" option and default to template
+                $this->newProjectPage();
+
+            } else if (PAGE==='manager/control_center.php') {
+                $this->checkEnabledForAll();
+            }
+
+        } else {
             // Dev projects force single user into owner role (if template has one) - works more flexibly than query string "new" with new project requests
             global $Proj;
             if ($Proj->project['status'] > 0) return;
-            $defaultRole = $this->getSystemSetting('project-owner-role-name');
-            if ($defaultRole!=='') {
-                $defaultRoleId = 0;
-                $r1 = $this->query("select role_id from redcap_user_roles where project_id=? and role_name=? limit 1", [ $project_id, $defaultRole ]);
-                if ($r1->num_rows > 0) {
-                    while($row = $r1->fetch_assoc()){
-                        $defaultRoleId = $row['role_id'];
-                    }
-                }
-                $r2 = $this->query("select if(role_id is null, 0, 1) as in_role from redcap_user_rights where project_id=? ", [ $project_id ]);
-                $nInRole = 0;
-                $nUsers = $r2->num_rows;
-                while($row = $r2->fetch_assoc()){
-                    $nInRole += $row['in_role'];
-                }
-                // if default role present and project has single user not in that role, then add them
-                if ($defaultRoleId > 0 && $nUsers==1 && $nInRole==0) {
-                    $this->moveUserToDefaultRole($defaultRoleId);
-                }
+            $this->userRoleAssignment($project_id);
+        
+            if (PAGE==='manager/project.php') {
+                ?>
+                <style type="text/css">
+                    .config-instruction { display: none; }
+                </style>
+                <?php
             }
         }
     }
 
-    protected function moveUserToDefaultRole($defaultRoleId) {
+    protected function newProjectPage() {
+        ?>
+        <script type='text/javascript'>
+            $(document).ready(function() {
+                $('#project_template_radio0').closest('div').hide(); 
+                setTimeout(function(){
+                    $('#project_template_radio1').val([1]).change();
+                }, 200);
+            });
+        </script>
+        <?php
+    }
+
+    protected function userRoleAssignment($project_id): void {
+        // do nothing when >1 user or user is in role
+        $result = $this->query("select count(*) as n_users, count(role_id) as n_in_role from redcap_user_rights where project_id=? ", [ $project_id ]);
+        $row = $result->fetch_assoc();
+        $nUsers = $row['n_users'];
+        $nInRole = $row['n_in_role'];
+        if ($nUsers>1 || $nInRole>0) return;
+
+        $defaultRoles = $this->getSystemSetting('project-owner-role-name');
+        $defaultRoles = array_values(array_filter($defaultRoles, function($roleName) {
+            return !is_null($roleName) && !empty(trim($roleName)); 
+        }));
+
+        foreach ($defaultRoles as $roleName) {
+            $result = $this->query("select role_id from redcap_user_roles where project_id=? and role_name=? limit 1", [ $project_id, $roleName ]);
+            if ($result->num_rows===0) continue;
+            
+            $row = $result->fetch_assoc();
+            $roleId = $row['role_id'];
+            if ($this->moveUserToDefaultRole($roleId)) {
+                break;
+            }
+        }
+    }
+
+    protected function moveUserToDefaultRole($defaultRoleId): bool {
         $sql = "update redcap_user_rights set role_id=? where project_id=? and role_id is null limit 1"; // already checked project has only one user - the creator/requestor
         if ($this->query($sql, [ $defaultRoleId, PROJECT_ID ])) {
             \REDCap::logEvent('External Module "'.$this->getModuleName().'": Assign user to default role','role_id = '.$defaultRoleId, sprintf(str_replace('?','%s',$sql), $defaultRoleId, PROJECT_ID));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * checkEnabledForAll()
+     * Highlight when module not enabled for all projects
+     * @return void
+     */
+    protected function checkEnabledForAll(): void {
+        if (!$this->getSystemSetting('enabled')) {
+            ?>
+            <style type="text/css">
+                .config-instruction { color: red; font-weight: bold; }
+            </style>
+            <?php
+        }
+    }
+
+    /**
+     * redcap_module_configuration_settings
+     * Triggered when the system or project configuration dialog is displayed for a given module.
+     * Allows dynamically modify and return the settings that will be displayed.
+     * @param string $project_id, $settings
+     * @return array
+     */
+    public function redcap_module_configuration_settings($project_id, $settings) {
+        if (empty($project_id)) {
+            if (!$this->getSystemSetting('enabled')) {
+                foreach ($settings as $si => $sarray) {
+                    if ($sarray['key']=='enable-on-all') {
+                        $settings[$si]['hidden'] = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return $settings;
+    }
+
+    public function redcap_user_rights($project_id) {
+        if (!$this->getSystemSetting('hide-custom-rights')) return;
+        ?>
+        <style type="text/css">
+            #addUsersRolesDiv > div:nth-child(3) { display: none; }
+            #addUsersRolesDiv > div:nth-child(4) { display: none; }
+        </style>
+        <?php
+    }
+
+    public function redcap_every_page_before_render($project_id) {
+        if (defined('PAGE') 
+            && PAGE==='UserRights/index.php' 
+            && !empty($project_id)
+            && $this->getSystemSetting('hide-custom-rights') ) 
+        {
+            global $lang;
+            $lang['rights_162'] = ''; // "Give them custom user rights or assign them to a role."
         }
     }
 }
--- README.md
+++ README.md
@@ -3,29 +3,55 @@
 
 Luke Stevens, Murdoch Children's Research Institute https://www.mcri.edu.au
 
+[https://github.com/lsgs/redcap-hide-create-empty-project](https://github.com/lsgs/redcap-hide-create-empty-project)
 ********************************************************************************
 ## Summary
 
-When creating or requesting a new project in REDCap, users are presented with 
-three choices:
-* 0 Create an empty project (blank slate)
-* 2 Upload a REDCap project XML file (CDISC ODM format)?
-* 1 Use a template (choose one below) 
+**Hides the "Empty project (blank slate)" option when creating/requesting a new project** so users must select a template for their new project (or create from an ODM XML file).
 
-This External Module hides the "Create an empty project (blank slate)" option,  
-forcing the user to use a template (or upload an ODM XML).
+If user role names are configured at the system level then the user who creates/requests the project will be added to the first matching role in the project.
 
-### Default User Roles in Template Projects
+### Create New Project
 
-Setting up templates enables REDCap administrators to define suitable default 
-user roles, e.g. for the "Project Owner", "Data Entry", "Investigator", "Data 
-User" etc. This illustrates the User Roles functionality and encourages users 
-to make use of it rather than managing their project users' permissions 
-individually.
+When creating or requesting a new project in REDCap, users are presented with three choices:
+* Empty project (blank slate)
+* Upload a REDCap project XML file (CDISC ODM format)?
+* Clinical Data Mart: create a project and pull multiple records from Epic [where enabled]
+* Use a template (choose one below) 
 
-### System-Level Default User Role Name
+This External Module hides the "Empty project (blank slate)" option, forcing the user to use a template (or create from an ODM XML file).
 
-The module system-level configuration enables the administrator to specify the 
-name of a user role (the default is "Project Owner") to which the user that 
-creates or requests the project will be automatically assigned once a project 
-has been created from a template.
+#### Default behaviour
+
+<img alt="with empty project option" src="https://redcap.mcri.edu.au/surveys/index.php?pid=14961&__passthru=DataEntry%2Fimage_view.php&doc_id_hash=3544cbf7fb5e999c3e3122d41bc1c97dddb8524afce17dd04a5b65ea909d02e94713d13d06949bfd8fa45bed819cc3798238fb06c26ce2764e0f10efb608f25b&id=2215454&s=KSqRoICnLEuQBqR2&page=file_page&record=22&event_id=47634&field_name=thefile&instance=1" />
+
+#### With this module enabled
+
+<img alt="without empty project option" src="https://redcap.mcri.edu.au/surveys/index.php?pid=14961&__passthru=DataEntry%2Fimage_view.php&doc_id_hash=5788cf6e36a86a0aa1be5cdbe0dfb1b9bca3a61d7b673e570902f9ae0877dac8a5521e3ef998bade2ff5e4ce5ddad76de71181afb2dab04125e599af57cd5c66&id=2215456&s=h8XEFayAI83xNAqz&page=file_page&record=23&event_id=47634&field_name=thefile&instance=1" />
+
+### Template Projects: Default User Roles
+
+Set up each of your project templates with a range of suitable default user roles, e.g. for the "Project Owner", "Data Entry", "Investigator", "Data User" etc. 
+
+This illustrates the User Roles functionality and encourages users to make use of it rather than managing their project users' permissions individually.
+
+
+## System-Level Settings
+
+<img alt="module settings" src="https://redcap.mcri.edu.au/surveys/index.php?pid=14961&__passthru=DataEntry%2Fimage_view.php&doc_id_hash=bf95b261b4955e6421343803ddea5543a5d18dc4521d81f3a53d32de0c9c55402c66e60fcf1e29ff192430a343a0f2aabd6e14979d4f1c99c543008d70b944db&id=2215457&s=cvPJUys7fpuZVorb&page=file_page&record=24&event_id=47634&field_name=thefile&instance=1" />
+
+### Enable for All Projects
+
+This module is designed to be used with the "Enable module on all projects by default" option swithched *ON*.
+
+### Hide "Add with custom rights" Option
+
+Select this box to hide the "Add with custom rights" option on project User Rights pages. This further encourages use of user roles and discourages permission management at the individual user level.
+
+<img alt="no custom rights option" src="https://redcap.mcri.edu.au/surveys/index.php?pid=14961&__passthru=DataEntry%2Fimage_view.php&doc_id_hash=1e5e584cd178a67ba6d96bcd5cfafbd458b34234f62606c7e65029ee4193939fbbb41376441ca3ebbe5ce04487f89485314583e2e462bbf1dff1a29c6b90690a&id=2215458&s=azeUGgLchCyAkEw5&page=file_page&record=25&event_id=47634&field_name=thefile&instance=1" />
+
+### Default User Role Names
+
+The module system-level configuration enables the administrator to specify a series of user role names. The user that creates or requests the project will be automatically assigned to the first matched role in the new project.
+
+<img alt="single project user auto-assigned to role" src="https://redcap.mcri.edu.au/surveys/index.php?pid=14961&__passthru=DataEntry%2Fimage_view.php&doc_id_hash=0b845a82219ea7559aa909603a39ac66cb6b60cc27f0bed751860c3f712d2c25193385f4e1c7e2891120e2f90a4b849ed76eb699ba1b7cfc20ca9a8445f0434b&id=2215459&s=a4isvwY6Dg4esfbg&page=file_page&record=26&event_id=47634&field_name=thefile&instance=1" />
